1cedb5c212b3c2f2e7de27229cebf0b34c844ea2
Akash Sharma <lucky1612000@gmail.com>
2021-06-27 13:40:24 +0530

    added configuration for java client

--- src/main/java/com/akash/app/WebSocketConfiguration.java
+++ src/main/java/com/akash/app/WebSocketConfiguration.java
@@ -5,20 +5,28 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.RequestUpgradeStrategy;
+import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @SpringBootConfiguration
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stomp-endpoint").setAllowedOriginPatterns("http://*").withSockJS();
-    }
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/stomp-endpoint").setAllowedOriginPatterns("http://*").withSockJS();
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
+		// extra configuration for java client
+		RequestUpgradeStrategy upgradeStrategy = new TomcatRequestUpgradeStrategy();
+		registry.addEndpoint("/stomp-endpoint").setHandshakeHandler(new DefaultHandshakeHandler(upgradeStrategy))
+				.setAllowedOriginPatterns("http://*", "ws://*");
+	}
 
-    }
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.enableSimpleBroker("/topic");
+		registry.setApplicationDestinationPrefixes("/app");
+
+	}
 }
